29b3d750992066e381ad80973161b34a783bbe64
Ivan Nikitin <vantoozz@gmail.com>
2011-05-31 14:45:32 +0400

    + hidden select

--- js/jquery.styled-dropdown.js
+++ js/jquery.styled-dropdown.js
@@ -1,14 +1,14 @@
 (function ($) {
-	$.fn.styledDropdown=function(){
-		
-		var selectedOption=this.find('option:selected');
-  		
-  		var selected = {
-                id: selectedOption.val(),
-                title: selectedOption.text()
-            }; 
+    $.fn.styledDropdown = function () {
 
-		var options = new Array();
+        var selectedOption = this.find('option:selected');
+
+        var selected = {
+            id: selectedOption.val(),
+            title: selectedOption.text()
+        };
+
+        var options = new Array();
         this.find('option').each(function () {
             var $this = $(this);
             options.push({
@@ -18,10 +18,10 @@
         });
 
 
-		var styledDropdownID='syledDropdown_'+Math.floor(Math.random()*1000)+'_'+new Date().getMilliseconds();
-        var html = '<div class="styledDropdown" id="'+styledDropdownID+'">'
-        		 + '<div class="styledDropdown_selectedOption" id="selectedOption_'+styledDropdownID+'" data-id="' + selected.id + '">' + selected.title + '</div>'
-        		 + '<div class="styledDropdown_optionsWrapper" id="optionsWrapper_'+styledDropdownID+'">'
+        var styledDropdownID = 'syledDropdown_' + Math.floor(Math.random() * 1000) + '_' + new Date().getMilliseconds();
+        var html = '<div class="styledDropdown" id="' + styledDropdownID + '">'
+        		 + '<div class="styledDropdown_selectedOption" id="selectedOption_' + styledDropdownID + '" data-id="' + selected.id + '">' + selected.title + '</div>'
+        		 + '<div class="styledDropdown_optionsWrapper" id="optionsWrapper_' + styledDropdownID + '">'
         		 + '<ul>';
         var i;
         for (i in options) {
@@ -31,13 +31,21 @@
              + '</div>'
              + '</div>';
 
+        var clone = this.clone();
+
         this.replaceWith(html);
 
-			
-		var $styledDropdown=$('#'+styledDropdownID);
+
+        var $styledDropdown = $('#' + styledDropdownID);
         var $optionsWrapper = $styledDropdown.find('div.styledDropdown_optionsWrapper');
         var $selectedOption = $styledDropdown.find('div.styledDropdown_selectedOption');
-        
+
+        $styledDropdown.prepend(clone);
+
+        var $hiddenSelect = $styledDropdown.find('select')
+        $hiddenSelect.hide();
+        var $hiddenOptions = $hiddenSelect.find('option');
+
 
         $selectedOption.click(function (event) {
             event.stopPropagation();
@@ -49,13 +57,16 @@
             var $this = $(this);
             $selectedOption.attr('data-id', $this.attr('data-id')).html($this.html()).toggleClass('droped');
             $optionsWrapper.toggle();
+            $hiddenOptions.attr('selected', '');
+            $hiddenSelect.find('option[value=' + $this.attr('data-id') + ']').attr('selected', 'selected').val($this.attr('data-id'));
+            $hiddenSelect.trigger('change')
         });
 
         $('body').click(function (event) {
             if ($selectedOption.hasClass('droped')) {
                 var needtohide = false;
                 if (event.srcElement) {
-                    if ('selectedOption_'+styledDropdownID != event.srcElement.id && 'optionsWrapper_'+styledDropdownID != event.srcElement.id) needtohide = true;
+                    if ('selectedOption_' + styledDropdownID != event.srcElement.id && 'optionsWrapper_' + styledDropdownID != event.srcElement.id) needtohide = true;
                 }
                 else needtohide = true;
                 if (needtohide) {
@@ -63,7 +74,7 @@
                     $selectedOption.toggleClass('droped');
                 }
             }
-        });		
-		
-	};	
+        });
+
+    };
 })(jQuery);
