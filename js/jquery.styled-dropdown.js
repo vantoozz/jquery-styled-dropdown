9d881847fbf20eab78aa3a5d7df25677a32d4426
Ivan Nikitin <vantoozz@gmail.com>
2011-05-31 13:37:55 +0400

    + dropdown

---
+++ js/jquery.styled-dropdown.js
@@ -0,0 +1,69 @@
+(function ($) {
+	$.fn.styledDropdown=function(){
+		
+		var selectedOption=this.find('option:selected');
+  		
+  		var selected = {
+                id: selectedOption.val(),
+                title: selectedOption.text()
+            }; 
+
+		var options = new Array();
+        this.find('option').each(function () {
+            var $this = $(this);
+            options.push({
+                id: $this.val(),
+                title: $this.text()
+            });
+        });
+
+
+		var styledDropdownID='syledDropdown_'+Math.floor(Math.random()*1000)+'_'+new Date().getMilliseconds();
+        var html = '<div class="styledDropdown" id="'+styledDropdownID+'">'
+        		 + '<div class="styledDropdown_selectedOption" id="selectedOption_'+styledDropdownID+'" data-id="' + selected.id + '">' + selected.title + '</div>'
+        		 + '<div class="styledDropdown_optionsWrapper" id="optionsWrapper_'+styledDropdownID+'">'
+        		 + '<ul>';
+        var i;
+        for (i in options) {
+            html += '<li data-id="' + options[i].id + '">' + options[i].title + '</li>';
+        }
+        html += '</ul>'
+             + '</div>'
+             + '</div>';
+
+        this.replaceWith(html);
+
+			
+		var $styledDropdown=$('#'+styledDropdownID);
+        var $optionsWrapper = $styledDropdown.find('div.styledDropdown_optionsWrapper');
+        var $selectedOption = $styledDropdown.find('div.styledDropdown_selectedOption');
+        
+
+        $selectedOption.click(function (event) {
+            event.stopPropagation();
+            $optionsWrapper.toggle();
+            $selectedOption.toggleClass('droped');
+        });
+
+        $optionsWrapper.find('li').click(function () {
+            var $this = $(this);
+            $selectedOption.attr('data-id', $this.attr('data-id')).html($this.html()).toggleClass('droped');
+            $optionsWrapper.toggle();
+        });
+
+        $('body').click(function (event) {
+            if ($selectedOption.hasClass('droped')) {
+                var needtohide = false;
+                if (event.srcElement) {
+                    if ('selectedOption_'+styledDropdownID != event.srcElement.id && 'optionsWrapper_'+styledDropdownID != event.srcElement.id) needtohide = true;
+                }
+                else needtohide = true;
+                if (needtohide) {
+                    $optionsWrapper.toggle();
+                    $selectedOption.toggleClass('droped');
+                }
+            }
+        });		
+		
+	};	
+})(jQuery);
